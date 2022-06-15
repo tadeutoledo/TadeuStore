2854aa86a106840f0b031a23dc1dcb423e071146
Tadeu Mathias <tmtadeu@gmail.com>
2022-06-15 00:49:57 -0300

    Adicionado testes iniciais de Domain

---
+++ TadeuStore.Tests/Domain/DomainTests.cs
@@ -0,0 +1,51 @@
+﻿using Bogus;
+using TadeuStore.Domain.ViewModels.Requisicao;
+using Xunit;
+using Bogus.Extensions.Brazil;
+using TadeuStore.Domain.Models.Enums;
+using TadeuStore.Domain.FluentValidation;
+using Bogus.DataSets;
+
+namespace TadeuStore.Tests.Domain
+{
+    [Collection(nameof(MockDomains))]
+    public class DomainTests
+    {
+        readonly MockDomains _mockDomains;
+
+        public DomainTests(
+            MockDomains mockDomains)
+        {
+            _mockDomains = mockDomains;
+        }
+
+        #region ViewModels
+
+        [Fact(DisplayName = "View Model Usuario - Sucesso")]
+        [Trait("Categoria", "View Model Tests")]
+        public void ViewModel_CadastrarUsuarioRequisicao_DeveValidarComSucesso()
+        {
+            // Arrange
+            var usuario = _mockDomains.GerarCadastrarUsuarioValido(1).First();
+            var validator = new UsuarioValidator();
+
+            // Act & Assert
+            Assert.True(validator.Validate(usuario).IsValid);
+        }
+
+        [Fact(DisplayName = "View Model Usuario - Falhar com senha fraca")]
+        [Trait("Categoria", "View Model Tests")]
+        public void ViewModel_CadastrarUsuarioRequisicao_DeveFalharDevidoASenhaFraca()
+        {
+            // Arrange
+            var usuario = _mockDomains.GerarCadastrarUsuarioValido(1).First();
+            usuario.Senha = "abc123!";
+            var validator = new UsuarioValidator();
+
+            // Act & Assert
+            Assert.False(validator.Validate(usuario).IsValid);
+        }
+
+        #endregion ViewModels
+    }
+}
--- TadeuStore.Tests/Domain/MockDomains.cs
+++ TadeuStore.Tests/Domain/MockDomains.cs
@@ -6,39 +6,36 @@ using TadeuStore.Services;
 using Bogus.Extensions.Brazil;
 using TadeuStore.Domain.Models.Enums;
 using Xunit;
+using TadeuStore.Domain.ViewModels.Requisicao;
 
-namespace TadeuStore.Tests.Services
+namespace TadeuStore.Tests.Domain
 {
-    [CollectionDefinition(nameof(MockServices))]
-    public class MockServices : ICollectionFixture<MockServices>, IDisposable
+    [CollectionDefinition(nameof(MockDomains))]
+    public class MockDomains : ICollectionFixture<MockDomains>, IDisposable
     {
         public readonly AutoMocker Mocker;
-        public readonly UsuarioService UsuarioService;
-        public readonly AplicativoService AplicativoService;
 
-        public MockServices()
+        public MockDomains()
         {
             Mocker = new AutoMocker();
-            UsuarioService = Mocker.CreateInstance<UsuarioService>();
-            AplicativoService = Mocker.CreateInstance<AplicativoService>();
         }
 
-        public IEnumerable<Usuario> GerarUsuariosValidos(int quantidade, bool senhaEncriptada = false)
+        public IEnumerable<CadastrarUsuarioRequisicaoViewModel> GerarCadastrarUsuarioValido(int quantidade, bool senhaEncriptada = false)
         {
             var genero = new Faker().PickRandom<Name.Gender>();
 
-            var usuario = new Faker<Usuario>("pt_BR")
+            var senhaSegura = "Abc!@1";
+
+            var usuario = new Faker<CadastrarUsuarioRequisicaoViewModel>("pt_BR")
                 .StrictMode(true)
                 .RuleFor(c => c.Nome, (f, c) => f.Name.FirstName(genero))
                 .RuleFor(c => c.Email, (f, c) => f.Internet.Email(c.Nome.ToLower()))
-                .RuleFor(c => c.Senha, (f, c) => senhaEncriptada ? BCrypt.Net.BCrypt.HashPassword(f.Internet.Password(15)) : f.Internet.Password(15))
+                .RuleFor(c => c.Senha, (f, c) => senhaEncriptada ? BCrypt.Net.BCrypt.HashPassword(f.Internet.Password(10, prefix: senhaSegura)) : f.Internet.Password(10, prefix: senhaSegura))
                 .RuleFor(c => c.Cpf, (f, c) => f.Person.Cpf(false))
                 .RuleFor(c => c.DataNascimento, (f, c) => f.Date.Past(80, DateTime.Now.AddYears(-20)))
                 .RuleFor(c => c.Sexo, (f, c) => (TipoSexo)genero)
                 .RuleFor(c => c.Endereco, (f, c) => f.Address.StreetAddress())
-                .RuleFor(c => c.Complemento, (f, c) => f.Address.FullAddress())
-                .RuleFor(c => c.CartoesCredito, (f, c) => new List<CartaoCredito>())
-                .RuleFor(c => c.Id, (f, c) => Guid.NewGuid());
+                .RuleFor(c => c.Complemento, (f, c) => f.Address.FullAddress());
 
             return usuario.Generate(quantidade);
         }
